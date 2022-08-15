6135fc0b981247f11d8f285353c8dfccd99f185b
BenSwartz <odanata410@gmail.com>
2022-08-15 09:28:59 -0500

    Products page now rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Products from './Products'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
