6978f5088c5eea8fc7f4050ce95adc6a4905a62e
Gabriel <gabrieljch929@gmail.com>
2022-08-22 14:34:40 -0500

    needsmorefix

--- src/components/Shipping.js
+++ src/components/Shipping.js
@@ -1,22 +1,84 @@
-import React, { useState, useEffect} from 'react';
 import Layout from "./Layout";
 import classes from "./Shipping.module.css";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { getCurrentData, clearCurrentData } from './../utils/auth'
+import CardProducts from "./CardProducts"
+import { nanoid } from "nanoid";
 
 const Shipping = () => {
-  
   const navigate = useNavigate();
-
   const nextNavigateHandler = () => {
-    navigate('/payment-method');
-  };
+    navigate('/payment');
+   };
+    const backNavigateHandler = () => {
+      navigate('/cart');
+     };
 
-  const nextDayShipping = () => {}
+    //  const backNavigateHandler = () => {
+    //   navigate('/cart');
+    //  };
+
+
+  const cartStoraged = getCurrentData('cart');
+  const [cartProducts, setCartproducts] = useState(cartStoraged || []);
+  const [total, setTotal] = useState(0);
+  const [taxes, setTaxes] = useState(0);
+  const [initailProducts, setInitialProducts] = useState([]);
+  const totalCost = (arr) => {
+    const totalSum = arr.reduce((prev, curr) => prev + curr.price * 1, 0);
+    setTotal(totalSum);
+    setTaxes(totalSum * 0.12);
+  };  
+
+  useEffect(() => {
+    if (!cartStoraged) {
+      setTotal(0);
+      setTaxes(0);
+    }
+    if (cartStoraged) {
+      totalCost(cartStoraged);
+      setCartproducts(cartStoraged);
+      setInitialProducts(cartStoraged);
+    }
+  }, [cartStoraged]);
+
+  const deleteAllProductsHandler = () => {
+    setCartproducts([]);
+    clearCurrentData();
+    setTotal(0);
+    setTaxes(0);
+  };
+  
+
 
   return (
     <Layout>
       <section>
         <div className={classes["shipping-main"]}>
+          {/* <div>
+          {cartProducts.length ? (
+                cartProducts.map((prod) => {
+                  return (
+                    <CardProducts
+                      key={nanoid()}
+                      name={prod.name}
+                      description={prod.description}
+                      price={prod.price}
+                      id={prod.id}
+                      cartProducts={cartProducts}
+                      setCartproducts={setCartproducts}
+                      setTotal={setTotal}
+                      setTaxes={setTaxes}
+                      qtn={prod.quantity}
+                      initailProducts={initailProducts}
+                    />
+                  );
+                })
+              ) : (
+                <p>there are no products</p>
+              )}
+          </div> */}
           <div className={classes["shipping-header"]}>
             <p>1. Shopping Cart</p>
             <p>2. Shipping Details</p>
@@ -80,19 +142,40 @@ const Shipping = () => {
                       </div>
                     </div>
                   </div>
-                  {/* <div>
-                    <input type="radio" />
-                    <span>Free Shipping</span>
-                  </div>
-                  <div>
-                    <input type="radio" />
-                    <span>Next Day Delivery</span>
-                  </div> */}
                 </div>
               </div>
             </div>
+            <div>
+            <div className={classes['cart-body-summary']}>
+              <h3>Summary</h3>
+              <div className={classes['cart-body-coupon']}>
+                Enter Coupon Code
+              </div>
+              <div className={classes['summary-detail']}>
+                <div className={classes['summary-payment']}>
+                  <div>Subtotal</div>
+                  <div>total</div>
+                </div>
+                <div className={classes['summary-payment']}>
+                  <div>Shipping</div>
+                  <div>Free</div>
+                </div>
+                <div className={classes['summary-payment']}>
+                  <div>Taxes</div>
+                  <div>taxes</div>
+                </div>
+              </div>
+              <div className={classes['summary-total']}>
+                <div>TOTAL</div>
+                <div>Total</div>
+              </div>
+            </div>
+          </div>
 
-            <div className={classes["shipping-body-summary"]}>
+
+            </div>
+
+            {/* <div className={classes["shipping-body-summary"]}>
               <h3>Summary</h3>
               <div>Product Name</div>
               <div>$300</div>
@@ -119,14 +202,16 @@ const Shipping = () => {
                   <div></div>$613
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
 
           <div className={classes["shipping-buttons"]}>
-            <button>Next</button>
+          <button onClick={backNavigateHandler}>Back</button>
+          <div className={classes["shipping-buttons"]}>
+            <button onClick={nextNavigateHandler}>Next</button>
             <button>Cancel</button>
           </div>
-        </div>
+          </div>
       </section>
     </Layout>
   );
