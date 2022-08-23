655b43b7ac5b4635943e69052d5daba22502b726
BenSwartz <odanata410@gmail.com>
2022-08-23 10:01:54 -0500

    products id finished just needs some more css.

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,19 +3,24 @@ import { useState, useEffect } from "react"
 import Layout from "./Layout"
 import { getAllProducts, deleteProduct, updateProduct } from "../api"
 import Pagination from "../pagination"
-import { getCurrentData } from "../utils/auth";
+import { getCurrentData, storeCurrentData } from "../utils/auth";
 import EditProduct from "./EditProduct";
+import classes from './Products.module.css'
 
 
 const Products = () => {
     const [products, setProducts] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
     const [currentPage, setCurrentPage] = useState(1)
-    const [productsPerPage] = useState(10)
+    const [productsPerPage] = useState(15)
     const [confirmSelect, setConfirmSelect] = useState(false)
     const [editProduct, setEditProduct] = useState(false)
     const [admin] = useState(getCurrentData('admin'))
     const [token] = useState(getCurrentData('token'))
+    const [cartQtn, setCartQtn] = useState(0);
+    const [singleProduct, setSingleProduct] = useState({});
+    const shoppingCart = getCurrentData('cart');
+    const [cart, setCart] = useState(shoppingCart || []);
 
     //Edit useStates
     const [currentProduct, setCurrentProduct] = useState({})
@@ -23,6 +28,10 @@ const Products = () => {
     const getAllIntialData = async () => {
         const allProducts = await getAllProducts()
         setProducts(allProducts)
+    }
+
+    const setShoppingCart = async () => {
+        getCurrentData('cart')
     }
 
 
@@ -44,19 +53,26 @@ const Products = () => {
         setConfirmSelect(false)
     }
 
+    const addToCartHandler = (e) => {
+        const [product] = products.filter(
+            (prod) => prod.id === +e.target.dataset.id
+        );
+        const productAdded = [];
+        const productSelected = { ...product, quantity: 1 };
+        setSingleProduct(productSelected);
+        productAdded.push(productSelected);
 
-    // function editHandler() {
-    //     updateProduct(
-    //         productName,
-    //         productDescription,
-    //         productPrice,
-    //         category_id,
-    //         inventory_id,
-    //         token,
-    //         product_id
-    //     )
-    //     setEditProduct(false)
-    // }
+        const isProductInCart = shoppingCart.find(
+            (prod) => prod.id == productSelected.id
+        );
+        if (isProductInCart) {
+            return;
+        }
+        setCart((prev) => [...prev, ...productAdded]);
+        setCartQtn((prev) => prev + 1);
+    };
+
+    storeCurrentData('cart', cart);
 
     {/* <form>
                 <input type='text' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search"></input>
@@ -72,35 +88,40 @@ const Products = () => {
 
     return (
         <Layout>
-            <h1>Products</h1>
+            <h1 className={classes['product-title']}>Products</h1>
             {editProduct ? <EditProduct product={currentProduct} /> : null}
-            {currentProducts.map((product) => {
-                return (
-                    <div key={`productHolder${product.id}`}>
-                        <h2>{product.name}</h2>
-                        <p>{product.description}</p>
-                        <p>${product.price}</p>
-                        {(admin && confirmSelect === false && editProduct === false) ? <div>
-                            <button onClick={() => {
-                                setEditProduct(true)
-                                setCurrentProduct(product)
-                            }}>edit</button>
-                            <button onClick={() => {
-                                setConfirmSelect(true)
-                            }}>delete</button>
-                        </div> : null}
-                        {confirmSelect ? <div>
-                            <button onClick={() => {
-                                deleteHandler(product.id)
-                                console.log(product.id)
-                            }}>Confirm</button>
-                            <button onClick={() => { setConfirmSelect(false) }}>Cancel</button>
-                        </div> : null}
-                    </div>
-                )
-            })
-            }
-            <Pagination productsPerPage={productsPerPage} totalProducts={products.length} paginate={paginate} />
+            <div className={classes['product-header']}>
+                {currentProducts.map((product) => {
+                    return (
+                        <div key={`productHolder${product.id}`} className={classes['product-card']}>
+                            <h2 className={classes['product-name']}>{product.name}</h2>
+                            <div className={classes['product-card-info']}>
+                                <p className={classes['product-card-description']}>{product.description}</p>
+                                <p className={classes['product-card-price']}>${product.price}</p>
+                            </div>
+                            <button className={classes['product-button']} data-id={product.id} onClick={addToCartHandler}>Purchase</button>
+                            {(admin && confirmSelect === false && editProduct === false) ? <div>
+                                <button className={classes['product-admin-button-edit']} onClick={() => {
+                                    setEditProduct(true)
+                                    setCurrentProduct(product)
+                                }}>edit</button>
+                                <button className={classes['product-admin-button-delete']} onClick={() => {
+                                    setConfirmSelect(true)
+                                }}>delete</button>
+                            </div> : null}
+                            {confirmSelect ? <div>
+                                <button onClick={() => {
+                                    deleteHandler(product.id)
+                                    console.log(product.id)
+                                }}>Confirm</button>
+                                <button onClick={() => { setConfirmSelect(false) }}>Cancel</button>
+                            </div> : null}
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <Pagination productsPerPage={productsPerPage} totalProducts={products.length} paginate={paginate} className={classes['product-paginate']} />
         </Layout >
     )
 }
